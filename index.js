00b9a88dca7cd11792464cb31e3aea006a56ae44
Kazushi Kawamura <wakaka2277@gmail.com>
2017-12-21 22:13:32 +0900

    fix bug

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ module.exports = {
     MyGlobal: true
   },
   rules: {
-    semi: [2, "always"]
+    semi: [2, 'always']
   },
-  env: {,
+  env: {
     browser: true,
     es6: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
   ],
   plugins: [
-    react
+    'react'
   ],
   rules: {
-    semi: [2, "always"],
-    quotes: [2, "single"],
-    object-curly-spacing: [2, "always"],
-    no-useless-return: 2,
-    padded-blocks: [2, "never"],
-    react/prefer-stateless-function: 2,
-    react/forbid-prop-types: 2
+    'semi': [2, 'always'],
+    'quotes': [2, 'single'],
+    'object-curly-spacing': [2, 'always'],
+    'no-useless-return': 2,
+    'padded-blocks': [2, 'never'],
+    'react/prefer-stateless-function': 2,
+    'react/forbid-prop-types': 2
   }
 };
