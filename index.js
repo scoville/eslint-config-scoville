ef105d883e9cd6b1a9db95c5b9b2c845c53a8740
Kazushi Kawamura <wakaka2277@gmail.com>
2017-12-21 22:06:06 +0900

    Add initial rules

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ module.exports = {
   globals: {
     MyGlobal: true
   },
-
   rules: {
     semi: [2, "always"]
+  },
+  env: {,
+    browser: true,
+    es6: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  plugins: [
+    react
+  ],
+  rules: {
+    semi: [2, "always"],
+    quotes: [2, "single"],
+    object-curly-spacing: [2, "always"],
+    no-useless-return: 2,
+    padded-blocks: [2, "never"],
+    react/prefer-stateless-function: 2,
+    react/forbid-prop-types: 2
   }
 };
