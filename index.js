63bfe4476f54532ff4dce869e368fe4309e16274
Kazushi Kawamura <wakaka2277@gmail.com>
2018-03-02 14:53:23 +0900

    Add no-unused-vars and jsx-quotes

--- index.js
+++ index.js
@@ -18,12 +18,14 @@ module.exports = {
   rules: {
     'semi': [2, 'always'],
     'quotes': [2, 'single'],
+    'jsx-quotes': [2, 'prefer-double'],
     'object-curly-spacing': [2, 'always'],
     'no-useless-return': 2,
+    'no-unused-vars': 2,
     'padded-blocks': [2, 'never'],
-    'react/prefer-stateless-function': [2, { 'ignorePureComponents': true }],
     'flowtype/semi': 2,
     // 'react/forbid-prop-types': 2,
+    'react/prefer-stateless-function': [2, { 'ignorePureComponents': true }],
     'comma-dangle': ['error', {
       arrays: 'always-multiline',
       objects: 'always-multiline',
